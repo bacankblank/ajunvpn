3045991bf3995a318d715ebec0ac1387b07e282e
bacankblank <87919670+bacankblank@users.noreply.github.com>
2021-07-26 15:36:30 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -7,16 +7,16 @@ echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
 echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
-echo -e "# Build Up By Ajun VPN                         #" | lolcat
+echo -e "# Build Up By Ajun VPN                          #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Thanks To                                     #" | lolcat
+echo -e "# Thanks To ( Haturnuhun Ka )                   #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Bokir Tampan as Ceu Odah Project                 #" | lolcat
-echo -e "# Padagang lotek Langganan Bokir      #" | lolcat
-echo -e "# Tanks to:                                     #" | lolcat
-echo -e "# Our Teams                                 #" | lolcat
-echo -e "#                                               #" | lolcat
-echo -e "#                                               #" | lolcat
-echo -e "#                                               #" | lolcat
+echo -e "# Bokir Tampan as Ceu Odah Project              #" | lolcat
+echo -e "# Padagang lotek Langganan Bokir                #" | lolcat
+echo -e "# Tukang Bubur Hayam (si Soglo)                 #" | lolcat
+echo -e "# Padagang Pindang                              #" | lolcat
+echo -e "# Join :                                        #" | lolcat
+echo -e "# T.me/ajunvpn_stores                           #" | lolcat
+echo -e "# T.me/friendly_teams                           #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "=================================================" | lolcat
