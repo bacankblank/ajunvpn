6add37065868e33d0b489e579a217705b1f87cdc
bacankblank <87919670+bacankblank@users.noreply.github.com>
2021-07-28 23:04:32 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -58,7 +58,7 @@ cat> /etc/v2ray/config.json << END
           ]
         },
         "wsSettings": {
-          "path": "/ajunvpn_stores",
+          "path": "/v2ray",
           "headers": {
             "Host": ""
           }
@@ -146,7 +146,7 @@ cat> /etc/v2ray/none.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/ajunvpn_stores",
+          "path": "/v2ray",
           "headers": {
             "Host": ""
           }
@@ -243,7 +243,7 @@ cat> /etc/v2ray/vless.json << END
           ]
         },
         "wsSettings": {
-          "path": "/ajunvpn_stores",
+          "path": "/v2ray",
           "headers": {
             "Host": ""
           }
@@ -330,7 +330,7 @@ cat> /etc/v2ray/vnone.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/ajunvpn_stores",
+          "path": "/v2ray",
           "headers": {
             "Host": ""
           }
