ff900f3d7398a298f1acc65a2e6b11f2ff3107ff
bacankblank <87919670+bacankblank@users.noreply.github.com>
2021-09-25 10:41:04 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -48,7 +48,7 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Full Account info"
+echo -e "Full Account Info"
 echo -e "SSH - OVPN & Websocket"
 echo -e "Username       : $Login"
 echo -e "Password       : $Pass"
