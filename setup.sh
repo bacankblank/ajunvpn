975e3dd83688a14c407404d221ff9fdff2a0bb15
bacankblank <87919670+bacankblank@users.noreply.github.com>
2021-10-10 17:50:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,15 +30,15 @@ echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+#wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/wadidaw.sh && chmod +x wadidaw.sh && screen -S wadidaw ./wadidaw.sh
 #install ssr
-wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+#wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+#wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+#wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+#wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
 wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 wget https://raw.githubusercontent.com/bacankblank/ajunvpn/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
