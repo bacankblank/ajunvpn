dc0ee03d449010f40dce1e9bd2c4ba2a9254c31d
bacankblank <87919670+bacankblank@users.noreply.github.com>
2021-11-21 22:07:23 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -7,13 +7,13 @@ echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
 echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
-echo -e "# Build Up By Ajun VPN                          #" | lolcat
+echo -e "# Build Up By HORASS                            #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Thanks To ( Haturnuhun Ka )                   #" | lolcat
+echo -e "# Thanks To                                     #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# Bokir Tampan as Ceu Odah Project              #" | lolcat
-echo -e "# Padagang lotek Langganan Bokir                #" | lolcat
-echo -e "# Tukang Bubur Hayam (si Soglo)                 #" | lolcat
+echo -e "# Horas Marolop Amsal Siregar (Owner Script)    #" | lolcat
+echo -e "# Tukang Bubur Hayam                            #" | lolcat
 echo -e "# Padagang Pindang                              #" | lolcat
 echo -e "# Join :                                        #" | lolcat
 echo -e "# T.me/ajunvpn_stores                           #" | lolcat
